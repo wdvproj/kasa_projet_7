a4e89a59e4df5986882cc4fc7b0870f948e42b05
wdvproj <dkevpro@outlook.fr>
2023-02-05 17:56:00 +0100

    Modification des attributs alt et lang

--- kasa_application/src/components/Card/index.jsx
+++ kasa_application/src/components/Card/index.jsx
@@ -3,7 +3,11 @@ import './style.css'
 function Card({ title, cover }) {
     return (
         <article className="card">
-            <img className="card__picture" src={cover} alt="logement" />
+            <img
+                className="card__picture"
+                src={cover}
+                alt="Intérieur du logement"
+            />
             <span className="card__title">{title}</span>
         </article>
     )
