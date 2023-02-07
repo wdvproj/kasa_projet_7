18f86060881cec95465c72874a8152781555ee25
wdvproj <dkevpro@outlook.fr>
2023-02-07 17:26:35 +0100

    Ajout du test d'ouverture de Collapse

---
+++ kasa_application/src/components/Collapse/index.test.js
@@ -0,0 +1,24 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import Collapse from './index.jsx'
+
+describe('Collapse', () => {
+    it('open then close', () => {
+        render(<Collapse title="Service" description="Good services" />)
+
+        const title = screen.getByText('Service')
+        const chevron = screen.getByRole('img')
+
+        expect(chevron.getAttribute('src')).toBe('chevron_down.svg')
+        // Au clic sur l'icône chevronDown, l'icône chevronUp apparaît ainsi que la description
+        fireEvent.click(title)
+        let description = screen.queryByText('Good services')
+        expect(chevron.getAttribute('src')).toBe('chevron_up.svg')
+        expect(description.textContent).toBe('Good services')
+
+        // Au clic sur l'icône chevronUp, l'icône chevronDown apparaît et la description n'apparaît pas
+        fireEvent.click(title)
+        const noDescription = screen.queryByText('Good services')
+        expect(chevron.getAttribute('src')).toBe('chevron_down.svg')
+        expect(noDescription).toBeNull()
+    })
+})
