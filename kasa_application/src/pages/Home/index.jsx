349123f2de429fa503493b441e07cb3510c02a9e
wdvproj <dkevpro@outlook.fr>
2023-02-11 19:05:56 +0100

    Ajout de la gestion de l'affichage des logements

--- kasa_application/src/pages/Home/index.jsx
+++ kasa_application/src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { Link, useLoaderData } from 'react-router-dom'
 import './style.css'
 import Layout from '../../components/Layout'
@@ -8,26 +9,35 @@ import photo from '../../assets/images/background_landscape_1.jpg'
 
 function Home() {
     const logements = useLoaderData()
+    const [isLogements, setIsLogements] = useState(false)
+
+    useEffect(() => {
+        if (logements) {
+            setIsLogements(true)
+        }
+    }, [logements])
 
     return (
-        <div>
-            <Layout>
-                <main className="home">
-                    <div className="home__banner">
-                        <Banner picture={photo} />
-                        <h1>Chez vous, partout et ailleurs</h1>
-                    </div>
-                    <section className="home__cards">
-                        {logements.map(({ id, title, cover }) => (
-                            <Link key={`link-${id}`} to={`/logement/${id}`}>
-                                <Card title={title} cover={cover} />
-                            </Link>
-                        ))}
-                    </section>
-                </main>
-            </Layout>
-            <Footer />
-        </div>
+        isLogements && (
+            <div>
+                <Layout>
+                    <main className="home">
+                        <div className="home__banner">
+                            <Banner picture={photo} />
+                            <h1>Chez vous, partout et ailleurs</h1>
+                        </div>
+                        <section className="home__cards">
+                            {logements.map(({ id, title, cover }) => (
+                                <Link key={`link-${id}`} to={`/logement/${id}`}>
+                                    <Card title={title} cover={cover} />
+                                </Link>
+                            ))}
+                        </section>
+                    </main>
+                </Layout>
+                <Footer />
+            </div>
+        )
     )
 }
 
