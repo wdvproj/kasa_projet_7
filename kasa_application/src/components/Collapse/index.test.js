c5ce24b17399513c057eb8c0de9efa63d87307d3
wdvproj <dkevpro@outlook.fr>
2023-02-16 18:06:10 +0100

    Modification des props

--- kasa_application/src/components/Collapse/index.test.js
+++ kasa_application/src/components/Collapse/index.test.js
@@ -2,8 +2,8 @@ import { render, screen, fireEvent } from '@testing-library/react'
 import Collapse from './index.jsx'
 
 describe('Collapse', () => {
-    it('open then close', () => {
-        render(<Collapse title="Service" description={['Good services']} />)
+    it('open then close', async () => {
+        render(<Collapse title="Service" children={<p>Good services</p>} />)
 
         const title = screen.getByText('Service')
         const chevron = screen.getByRole('img')
@@ -11,9 +11,9 @@ describe('Collapse', () => {
         expect(chevron.getAttribute('src')).toBe('chevron_down.svg')
         // Au clic sur l'icône chevronDown, l'icône chevronUp apparaît ainsi que la description
         fireEvent.click(title)
-        let description = screen.getByRole('list')
+        let description = screen.getByText('Good services')
         expect(chevron.getAttribute('src')).toBe('chevron_up.svg')
-        expect(description.textContent).toBe('Good services')
+        expect(description).toBeTruthy()
 
         // Au clic sur l'icône chevronUp, l'icône chevronDown apparaît et la description n'apparaît pas
         fireEvent.click(title)
